285961b21934a9621dc0689276b3d374eb7ad118
Mohammad Issawi <2000issawi@gmail.com>
2021-11-02 20:54:36 +0200

    set up renderer

---
+++ include/Renderer.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML/Window.hpp>
+#include<GL/glew.h>
+#define RENDERER Renderer::instance()
+namespace tat
+{
+    class Renderer
+    {
+    private:
+        Renderer();
+        sf::Window *window;
+
+    public:
+        static Renderer &instance();
+        void init(const char *title, int height, int width);
+        void render(std::function<void()> renderFunc);
+        void terminate();
+    };
+}
---
+++ src/Renderer.cpp
@@ -0,0 +1,47 @@
+#include <Renderer.h>
+using namespace tat;
+using namespace sf;
+using namespace std;
+Renderer::Renderer() {}
+Renderer &Renderer::instance()
+{
+    static Renderer renderer;
+    return renderer;
+}
+void Renderer::init(const char *title, int height, int width)
+{
+    ContextSettings ctxSettings;
+    ctxSettings.antialiasingLevel = 1;
+    ctxSettings.depthBits = 16;
+    ctxSettings.majorVersion = 4;
+    ctxSettings.minorVersion = 4;
+    ctxSettings.stencilBits = 8;
+    window = new Window(VideoMode(width, height), title, Style::Default, ctxSettings);
+}
+void Renderer::render(function<void()> renderFunc)
+{
+    while (1)
+    {
+        Event event;
+        while (window->pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case Event::Closed:
+                terminate();
+                return;
+            case Event::Resized:
+                //TODO
+                break;
+            default:
+                break;
+            }
+        }
+        renderFunc();
+        window->display();
+    }
+}
+void Renderer::terminate()
+{
+    window->close();
+}
